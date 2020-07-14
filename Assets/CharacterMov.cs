b4812d3e8d38ae8e6da19ae840c73dfe2495181f
Stefan Frunza <stefanedi@hotmail.com>
2020-07-14 22:59:40 +0300

    movement added

---
+++ Assets/CharacterMov.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CharacterMov : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    void FixedUpdate()
+    {
+           if(Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+           if(Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+          if(Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
